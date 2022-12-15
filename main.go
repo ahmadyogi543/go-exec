1be23ec7d6d02212fbd21df99fbb7a338e19cc72
Ahmad Yogi <ahmadyogi543@gmail.com>
2022-12-15 11:11:04 +0800

    update: using cors

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"github.com/ahmadyogi543/go-exec/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	app := gin.Default()
+	app.Use(cors.Default())
 	v1 := app.Group("/api/v1")
 
 	routes.Main(v1)
